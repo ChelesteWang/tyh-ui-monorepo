c6403185e2697c5441ea346a85de35f49b9780bb
15227241216 <1469442737@qq.com>
2022-03-16 23:44:13 +0800

    更新

--- packages/docs/src/main.ts
+++ packages/docs/src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import tyhUi2 from 'tyh-ui2'
+import 'tyh-ui2/style/index.css'
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(tyhUi2)
+  .mount('#app')
