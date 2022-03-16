97216c80fa670d476fdb9fc42f7f7a1656619101
15227241216 <1469442737@qq.com>
2022-03-16 23:57:46 +0800

    更新

--- packages/docs/src/main.ts
+++ packages/docs/src/main.ts
@@ -1,7 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import tyhUi2 from 'tyh-ui2'
-import 'tyh-ui2/style/index.css'
+import tyhUi2 from '@tyh-ui-monorepo/tyh-ui2'
+import '@tyh-ui-monorepo/tyh-ui2/style/index.css'
+
+console.log(tyhUi2)
+// import 'tyh-ui2/style/index.css'
+
+// const tyhUi2 = require('@tyh-ui-monorepo/tyh-ui2')
 
 createApp(App)
   .use(tyhUi2)
