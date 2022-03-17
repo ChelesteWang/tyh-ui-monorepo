99bf35f19809e42077e12e5e2c3ba29a800a020c
Tyh2001 <1469442737@qq.com>
2022-03-17 10:20:53 +0800

    更新

--- packages/docs/src/main.ts
+++ packages/docs/src/main.ts
@@ -1,12 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import tyhUi2 from '@tyh-ui-monorepo/tyh-ui2'
-import '@tyh-ui-monorepo/tyh-ui2/style/index.css'
-
-console.log(tyhUi2)
-// import 'tyh-ui2/style/index.css'
-
-// const tyhUi2 = require('@tyh-ui-monorepo/tyh-ui2')
+import tyhUi2 from '@tyh-ui2'
+import '@tyh-ui2/style/index.css'
 
 createApp(App)
   .use(tyhUi2)
