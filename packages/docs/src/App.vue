9914e788e936f3a74ad3a3fa79b12187462437aa
Tyh2001 <1469442737@qq.com>
2022-03-17 11:36:29 +0800

    更新

--- packages/docs/src/App.vue
+++ packages/docs/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <h1>hello</h1>
-  <tyh-button type="primary">主要按钮</tyh-button>
+ <tyh-button>默认按钮</tyh-button>
+<tyh-button type="primary">主要按钮</tyh-button>
+<tyh-button type="success">成功按钮</tyh-button>
+<tyh-button type="danger">危险按钮</tyh-button>
+<tyh-button type="warning">警告按钮</tyh-button>
 </template>
 
 <script lang="ts" setup>
